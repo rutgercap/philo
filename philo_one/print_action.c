5186e58566f1113e9a4956f7b559afb5b8ee2288
Rutger Cappendijk <rcappend@f1r2s20.codam.nl>
2022-03-25 09:21:04 +0100

    replaced write with printf

--- philo_one/print_action.c
+++ philo_one/print_action.c
@@ -6,7 +6,7 @@
 /*   By: rutgercappendijk <rutgercappendijk@stud      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:13:35 by rutgercappe   #+#    #+#                 */
-/*   Updated: 2022/03/24 16:49:56 by rcappend      ########   odam.nl         */
+/*   Updated: 2022/03/25 09:20:52 by rcappend      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	print_action(t_philo *philo, char *msg)
 {
-	int64_t	time;
+	uint64_t	time;
 
 	pthread_mutex_lock(&philo->rules->write_mut);
 	if (philo->rules->dead)
@@ -23,11 +23,7 @@ int	print_action(t_philo *philo, char *msg)
 		return (EXIT_FAILURE);
 	}
 	time = time_since_start(philo->rules);
-	ft_putnbr_fd(time, 1);
-	ft_putstr_fd(": ", 1);
-	ft_putnbr_fd(philo->id, 1);
-	ft_putchar_fd(' ', 1);
-	ft_putendl_fd(msg, 1);
+	printf("%llu: %d %s\n", time, philo->id, msg);
 	pthread_mutex_unlock(&philo->rules->write_mut);
 	return (EXIT_SUCCESS);
 }
